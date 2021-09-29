e8afc9d9e14f0d4270153ebc221b9421c001f5aa
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2021-09-30 09:19:02 +0930

    added models folder with customer detail and venue model

---
+++ COVIDCheckinWebApp/Models/VenueDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVIDCheckinWebApp.Models
+{
+    public class VenueDetail
+    {
+        public string venueName { get; set; }
+        public string venueAddress { get; set; }
+        public string venuePhone { get; set; }
+        public string ownerName { get; set; }
+
+    }
+}
