c791c60747ea0d62bdc63bcb75d738d60443c7fb
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2021-09-30 15:57:52 +0930

    some updates to models, and have basic searching for users via venues and vice versa

--- COVIDCheckinWebApp/Models/CustomerDetail.cs
+++ COVIDCheckinWebApp/Models/CustomerDetail.cs
@@ -10,5 +10,7 @@ namespace COVIDCheckinWebApp.Models
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string phoneNumber { get; set; }
+        public DateTime logDate { get; set; }
+        public string uniqueId { get; set; }
     }
 }
--- COVIDCheckinWebApp/Models/VenueDetail.cs
+++ COVIDCheckinWebApp/Models/VenueDetail.cs
@@ -7,10 +7,12 @@ namespace COVIDCheckinWebApp.Models
 {
     public class VenueDetail
     {
+        public int venueId { get; set; }
         public string venueName { get; set; }
         public string venueAddress { get; set; }
         public string venuePhone { get; set; }
         public string ownerName { get; set; }
+        public DateTime logDate { get; set; }
 
     }
 }
